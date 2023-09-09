95582a4cb7f79d95a1835c0f892da67188aec662
valonmulolli <valonmulolli@gmail.com>
2023-09-09 17:02:07 +0200

    SearchScreen

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -1,27 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { tracks } from '../../../assets/data/tracks';
+import TrackListItem from '../../components/TrackListItem';
 
 export default function Favorites() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Tab Two</Text>
-			<View style={styles.separator} />
-		</View>
+		<FlatList
+			data={tracks}
+			renderItem={({ item }) => <TrackListItem track={item} />}
+			showsVerticalScrollIndicator={false}
+		/>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: 'bold',
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: '80%',
-	},
-});
+const styles = StyleSheet.create({});
--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,29 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import TrackListItem from '../../components/TrackListItem';
+import { tracks } from '../../../assets/data/tracks';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+import COLORS from '../../constants/Colors';
+import { useState } from 'react';
 
 export default function Search() {
+	const [search, setSearch] = useState(' ');
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Tab Two</Text>
-			<View
-				style={styles.separator}
+		<SafeAreaView>
+			<View style={styles.header}>
+				{/* Header */}
+				<FontAwesome name='search' size={22} color={COLORS.gray2} />
+				<TextInput
+					value={search}
+					onChangeText={setSearch}
+					placeholder='Search'
+					style={styles.Input}
+				/>
+				<Text
+					onPress={() => setSearch('')}
+					style={{ fontSize: 16, color: COLORS.white }}
+				>
+					Cancel
+				</Text>
+			</View>
+			<FlatList
+				data={tracks}
+				renderItem={({ item }) => <TrackListItem track={item} />}
+				showsVerticalScrollIndicator={false}
 			/>
-		</View>
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+	header: {
+		flexDirection: 'row',
 		alignItems: 'center',
-		justifyContent: 'center',
+		padding: 10,
+	},
+	Input: {
+		flex: 1,
+		backgroundColor: COLORS.lightWhite,
+		padding: 8,
+		marginHorizontal: 10,
+		borderRadius: 5,
+		color: COLORS.gray,
 	},
 	title: {
 		fontSize: 20,
 		fontWeight: 'bold',
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: '80%',
 	},
 });
