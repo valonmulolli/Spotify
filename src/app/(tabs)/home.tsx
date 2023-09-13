644f0d63911fed6819bfc3244a1217b039861c9d
valonmulolli <valonmulolli@gmail.com>
2023-09-13 17:20:48 +0200

    Recomendation Query

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,9 +1,53 @@
-import { View, StyleSheet, FlatList } from 'react-native';
-import { tracks } from '../../../assets/data/tracks';
+import {
+	View,
+	Text,
+	FlatList,
+	ActivityIndicator,
+} from 'react-native';
 import TrackListItem from '../../components/TrackListItem';
-import Player from '../../components/Player.1';
+import { gql, useQuery } from '@apollo/client';
+import COLORS from '../../constants/Colors';
+
+const query = gql`
+	query MyQuery($genres: String!) {
+		recommendations(seed_genres: $genres) {
+			tracks {
+				id
+				name
+				preview_url
+				artists {
+					id
+					name
+				}
+				album {
+					id
+					name
+					images {
+						url
+						height
+						width
+					}
+				}
+			}
+		}
+	}
+`;
 
 export default function Home() {
+	const { data, loading, error } = useQuery(query, {
+		variables: { genres: 'hip-hop,house,pop' },
+	});
+
+	if (loading) {
+		return <ActivityIndicator />;
+	}
+
+	if (error) {
+		return <Text style={{color : COLORS.white}}>Failed to fetch recommendations</Text>;
+	}
+
+	const tracks = data?.recommendations?.tracks || [];
+
 	return (
 		<FlatList
 			data={tracks}
@@ -12,5 +56,3 @@ export default function Home() {
 		/>
 	);
 }
-
-const styles = StyleSheet.create({});
